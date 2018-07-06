5e7e568ca267e529bcb05bb7c36d42453ff6e1f2
Pablo <pablo.cedeno@gmail.com>
2018-07-05 22:15:55 -0600

    Another commit for testing

--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -6,7 +6,7 @@ public class Main {
       System.out.println("Hello world");
       String[] values = {"hello", " ", "world"};
       for (String value : values) {
-        System.out.print(value);
+        System.out.print(value + " 1");
       }
     }
 }
